c3e3b8c08be8b1116d59ff8c627ac54e7231412d
Łukasz Biały <lukasz@keios.eu>
2015-12-06 12:22:48 +0100

    added task descriptions as comments in task sources

--- src/main/scala/eu/keios/AdventOfCode/Task1b.scala
+++ src/main/scala/eu/keios/AdventOfCode/Task1b.scala
@@ -1,5 +1,15 @@
 package eu.keios.AdventOfCode
 
+//  --- Part Two ---
+//
+//  Now, given the same instructions, find the position of the first character that causes him to enter the basement (floor -1). The first character in the instructions has position 1, the second character has position 2, and so on.
+//
+//  For example:
+//
+//  ) causes him to enter the basement at character position 1.
+//  ()()) causes him to enter the basement at character position 5.
+//  What is the position of the character that causes Santa to first enter the basement?
+
 import scala.annotation.tailrec
 
 object Task1b extends Task1b with Task1a {
--- src/main/scala/eu/keios/AdventOfCode/Task4b.scala
+++ src/main/scala/eu/keios/AdventOfCode/Task4b.scala
@@ -1,5 +1,9 @@
 package eu.keios.AdventOfCode
 
+//  --- Part Two ---
+//
+//  Now find one that starts with six zeroes.
+
 object Task4b extends Task4a {
 
   override val hashStartKey = "000000"
